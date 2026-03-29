47c3763b942fa6808f11c1bb8f9055fde404eebb
ishimwejeanluc <ljeanluc394@gmail.com>
2026-03-29 18:40:02 +0200

    first commit

---
+++ terraform/main.tf
@@ -0,0 +1,122 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 4.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+locals {
+  ssh_user         = "ec2-user"
+  private_key_path = "${path.module}/${var.key_name}.pem"
+}
+
+resource "tls_private_key" "ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_sensitive_file" "private_key" {
+  filename        = local.private_key_path
+  content         = tls_private_key.ssh.private_key_pem
+  file_permission = "0400"
+}
+
+resource "aws_key_pair" "this" {
+  key_name   = var.key_name
+  public_key = tls_private_key.ssh.public_key_openssh
+}
+
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "${var.project_name}-sg"
+  description = "Web access for ${var.project_name}"
+  vpc_id      = data.aws_vpc.default.id
+
+  tags = {
+    Name = "${var.project_name}-sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
+  security_group_id = aws_security_group.web_sg.id
+  cidr_ipv4         = var.allowed_ssh_cidr
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
+  security_group_id = aws_security_group.web_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.web_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
+resource "aws_instance" "web" {
+  ami                         = data.aws_ami.amazon_linux_2.id
+  instance_type               = var.instance_type
+  key_name                    = aws_key_pair.this.key_name
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  subnet_id                   = data.aws_subnets.default.ids[0]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "${var.project_name}-web"
+  }
+}
+
+resource "local_file" "ansible_inventory" {
+  filename = "${path.module}/../ansible/inventory.ini"
+  content = templatefile("${path.module}/templates/inventory.tmpl", {
+    public_ip       = aws_instance.web.public_ip
+    ssh_user        = local.ssh_user
+    private_key_path = local.private_key_path
+  })
+}
